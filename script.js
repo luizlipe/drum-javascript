0dee55e67aa474a2d9473aebd7a47bf1421fd303
luizlipe <felipe250718@gmail.com>
2024-11-26 08:15:46 -0300

    adicionado função para tocar o audio

---
+++ script.js
@@ -0,0 +1,12 @@
+document.body.addEventListener('keyup', (event) =>{
+   playSound(event.code.toLocaleLowerCase()) 
+})
+
+function playSound(sound){
+    let audioElement = document.querySelector(`#s_${sound}`)
+
+    if(audioElement) {
+        audioElement.play()
+    }
+}
+
